f7db5be7f1954f4175299c8939f94b1be871cce0
Lenovo laptop <1457652314@qq.com>
2021-06-01 16:06:31 +0800

    normal

---
+++ 基础课/1.基础算法/双指针/最长连续不重复子序列.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int N = 1e5 + 5;
+int a[N];
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n;i++)
+        scanf("%d", &a[i]);
+    int i = 0, j = 0;
+    
+    return 0;
+}
