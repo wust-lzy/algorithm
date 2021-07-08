f7bcbcd6a017692d21cab5cf2bc2b53dcb423ad9
zhiying <1457652314@qq.com>
2021-07-09 00:35:39 +0800

    111

--- 草稿.cpp
+++ 草稿.cpp
@@ -3,14 +3,10 @@
 #include<cstring>
 using namespace std;
 
-<<<<<<< HEAD
-const int N = 1e5+5;
-int a[N];
-=======
 const int N = 1e5 + 10;
 
->>>>>>> b19ce4003fac9cd61de97b2db1fd4f7fff01d118
 int main()
 {
+
     return 0;
 }
