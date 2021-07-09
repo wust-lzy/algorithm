6caf956e31de44e0a6623803dc6813cecbeed104
Zhiying Lin <zhiying.lin@valtech.com>
2021-07-09 15:15:20 +0800

    222

--- 基础课/1.基础算法/快速排序/快速排序.cpp
+++ 基础课/1.基础算法/快速排序/快速排序.cpp
@@ -29,11 +29,10 @@ int main()
     int n;
     scanf("%d", &n);
     for (int i = 0; i < n; i++)
-    {
         scanf("%d", &q[i]);
-    }
     q_sort(q, 0, n - 1);
-    for (int i = 0; i < n;i++){
+    for (int i = 0; i < n;i++)
+    {
         printf("%d", q[i]);
         if(i!=n-1)
             printf(" ");
--- 草稿.cpp
+++ 草稿.cpp
@@ -7,5 +7,6 @@ const int N = 1e5 + 10;
 
 int main()
 {
+    cout<<123;
     return 0;
 }
