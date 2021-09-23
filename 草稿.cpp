39bb552cdeb94052063bae260a8bc47a5b538151
zhiying <1457652314@qq.com>
2021-09-23 22:47:53 +0800

    fix

---
+++ 草稿.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST ios::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+const int N = 100010;
+
+
+void output(vector<int>&p)
+{
+    for (int i = 0; i < p.size();i++){
+        printf("%d", p[i]);
+        if(i!=p.size()-1)
+            printf(" ");
+    }
+}
+int main()
+{
+    return 0;
+}
